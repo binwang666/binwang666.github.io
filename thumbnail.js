29a1310148ebeaed277c2b2df51bbf2664557970
binwang666 <99001185+binwang666@users.noreply.github.com>
2022-03-21 12:22:01 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -4,9 +4,90 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     targetImage.addEventListener("click", function () {
         if (targetImage.classList.contains("small")) {
-            targetImage.classList.remove("small");
+            targetImage.classList.remove("small")
         } else {
             targetImage.classList.add("small");
+        }
+    })
+    let targetImage2 = document.querySelector("#smart-image2");
+
+    targetImage2.addEventListener("click", function () {
+        if (targetImage2.classList.contains("small")) {
+            targetImage2.classList.remove("small")
+        } else {
+            targetImage2.classList.add("small");
+        }
+    })
+    let targetImage3 = document.querySelector("#smart-image3");
+
+    targetImage3.addEventListener("click", function () {
+        if (targetImage3.classList.contains("small")) {
+            targetImage3.classList.remove("small")
+        } else {
+            targetImage3.classList.add("small");
+        }
+    })
+    let targetImage4 = document.querySelector("#smart-image4");
+
+    targetImage4.addEventListener("click", function () {
+        if (targetImage4.classList.contains("small")) {
+            targetImage4.classList.remove("small")
+        } else {
+            targetImage4.classList.add("small");
+        }
+    })
+    let targetImage5 = document.querySelector("#smart-image5");
+
+    targetImage5.addEventListener("click", function () {
+        if (targetImage5.classList.contains("small")) {
+            targetImage5.classList.remove("small")
+        } else {
+            targetImage5.classList.add("small");
+        }
+    })
+    let targetImage6 = document.querySelector("#smart-image6");
+
+    targetImage6.addEventListener("click", function () {
+        if (targetImage6.classList.contains("small")) {
+            targetImage6.classList.remove("small")
+        } else {
+            targetImage6.classList.add("small");
+        }
+    })
+    let targetImage7 = document.querySelector("#smart-image7");
+
+    targetImage7.addEventListener("click", function () {
+        if (targetImage7.classList.contains("small")) {
+            targetImage7.classList.remove("small")
+        } else {
+            targetImage.classList.add("small");
+        }
+    })
+    let targetImage8 = document.querySelector("#smart-image8");
+
+    targetImage8.addEventListener("click", function () {
+        if (targetImage8.classList.contains("small")) {
+            targetImage8.classList.remove("small")
+        } else {
+            targetImage8.classList.add("small");
+        }
+    })
+    let targetImage9= document.querySelector("#smart-image9");
+
+    targetImage9.addEventListener("click", function () {
+        if (targetImage9.classList.contains("small")) {
+            targetImage9.classList.remove("small")
+        } else {
+            targetImage9.classList.add("small");
+        }
+    })
+    let targetImage10 = document.querySelector("#smart-image10");
+
+    targetImage10.addEventListener("click", function () {
+        if (targetImage10.classList.contains("small")) {
+            targetImage10.classList.remove("small")
+        } else {
+            targetImage10.classList.add("small");
         }
     })
 })
